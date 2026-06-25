ea97dd569a7c1a37e1d38ce4c043a276dcbfbd3a
Shaza-Elshimy <shazahamdy011@gmail.com>
2026-06-25 13:22:16 +0300

    Broadcast messages to all connected clients

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-
 const WebSocket = require("ws");
 
 const server = new WebSocket.Server({
     port:8000
 });
 
+const clients=[];
 server.on("connection",(socket)=>{
+
     console.log("client Connected");
+
+    clients.push(socket);
+    console.log(clients.length);
+
     socket.on("message",(message)=>{
-        console.log(message.toString());
-        
-        socket.send(`You said:${message}`);
+
+        clients.forEach(client=>{
+            client.send(message.toString())
+
+        })
+   
+
     })
-    
+  
 });
